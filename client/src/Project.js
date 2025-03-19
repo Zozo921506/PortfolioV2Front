7e0b85ce75b0c970fabaedc61ac83f4b402d8930
Enzo <enzo.falla@epitech.eu>
2025-03-19 16:20:47 +0100

    Ajout de l'affichage de projets s'il y a une erreur api

--- client/src/Project.js
+++ client/src/Project.js
@@ -4,6 +4,15 @@ function Project() {
   const [displayProjects, setDisplayProjects] = useState([]);
   const [selectedProject, setSelectedProject] = useState(null);
   const [search, setSearch] = useState("");
+  const [errorApi, setErrorApi] = useState(false);
+  const apiProjects = [
+    {id: 1, name: "My cinema", description: "Durant ce projet, je devais faire un site de cinéma où l'on pouvait rechercher un film par nom/catégories, de pouvoir voir la liste des membres et de leurs grades. Ainsi que de pouvoir modifier ces derniers, de plus, on devait pouvoir ajouter une séance.", languages: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"], imageURL: "./Image/projets/cinema.png"},
+    {id: 2, name: "Puissance 4", description: "Durant ce projet, je devais refaire un jeu de société iconique qui est le puissance 4. Je devais le faire en module. L'objectif de ce jeu est très simple, le premier joueur qui aligne horizontalement ou verticalement ou en diagonnales 4 pions de la même couleur remporte la partie.", languages: ["HTML", "CSS", "JavaScript"], imageURL: "./Image/projets/puissance4.png"},
+    {id: 3, name: "E-commerce", description: "Durant ce projet, je devais faire un site ayant les fonctionnalités de e-commerce ayant pour contrainte de vérifier si l'utilisateur est majeur ou non. Si l'utilisateur est mineur, il ne peut pas créer de compte et accéder aux fonctionnalités qui sont la recherche par genre, par tranche d'âge et par localisation. Je devais également faire un système de connexion avec les informations de l'utilisateur.", languages: ["CSS", "ReactJs", "Symfony", "MySQL"], imageURL: "./Image/projets/e-commerce.png"},
+    {id: 4, name: "Twitter", description: "Durant ce projet, je devais refaire twitter et ses fonctionnalités que se soit de pouvoir se connecter, tweeter, retweeter, répondre au tweet et au messages, liker un tweet, un tweet avec 140 caractères maximum, pouvoir voir ses abonnés et ses abonnement et rechercher par tags de l'utilisateur.", languages: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"], imageURL: "./Image/projets/twitter.png"},
+    {id: 5, name: "Spotify", description: "Durant ce projet, je devais reproduire certaines fonctionnalités de spotify qui sont la recherche, les albums et leurs détails, les genres et leurs détails et les artistes et leurs détails.", languages: ["CSS", "ReactJs", "Docker"], imageURL: "./Image/projets/spotify.png"},
+    {id: 6, name: "PortfolioV1", description: "Ce projet est la première version de mon portfolio que j'ai effectué en première année", languages: ["HTML", "CSS", "JavaScript"], imageURL: "./Image/projets/portfolio.png"},
+  ];
 
   const getProjects = async() => {
     const url = "http://51.75.122.193/api/projects";
@@ -30,6 +39,9 @@ function Project() {
     catch (e)
     {
       console.error(e);
+      setErrorApi(true);
+      setDisplayProjects(apiProjects)
+      setSelectedProject(apiProjects[0]);
     }
   }
 
@@ -49,39 +61,65 @@ function Project() {
   }
 
   useEffect(() => {
-    getProjects();
-  }, []);
+    if (!errorApi)
+    {
+      getProjects();
+    }
+  });
   return (
     <div className='project-container'>
         <input placeholder='Rechercher des projets' onChange={(e) => inputChange(e)} value={search} id='searchbar'></input>
         <div className='display-project'>
             <div className='project-details'>
-              {selectedProject ? (
+              {errorApi ? (
               <>
-                <img src={"http://51.75.122.193".selectedProject.imageUrl} alt={selectedProject.name} className='selected-pic'></img>
-                <h4>Titre: {selectedProject.name}</h4>
-                <p><strong>Description:</strong> {selectedProject.description}</p>
-                <div className='project-languages'>
-                  <p><strong>Langages: </strong></p>
-                  {selectedProject.languages.map((language) => (
-                    <p key={language}>{language}</p>
-                  ))}
-                </div>
-                <p><strong>Ajouté: </strong>{formatDate(selectedProject.created_at)}</p>
-                <p><strong>Dernière modification: </strong>{formatDate(selectedProject.updated_at)}</p>
-              </>
-            ) : (
-              <p>Sélectionnez un projet pour voir les détails</p>
-            )}
+                {selectedProject ? (
+                <>
+                  <img src={selectedProject.imageURL} alt={selectedProject.name} className='selected-pic'></img>
+                  <h4>Titre: {selectedProject.name}</h4>
+                  <p><strong>Description:</strong> {selectedProject.description}</p>
+                  <div className='project-languages'>
+                    <p><strong>Langages: </strong></p>
+                    {selectedProject.languages.map((language) => (
+                      <p key={language}>{language}</p>
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <p>Sélectionnez un projet pour voir les détails</p>
+              )}
+              </>) : (
+                <>
+                  {selectedProject ? (
+                  <>
+                    <img src={`http://51.75.122.193"${selectedProject.imageUrl}`} alt={selectedProject.name} className='selected-pic'></img>
+                    <h4>Titre: {selectedProject.name}</h4>
+                    <p><strong>Description:</strong> {selectedProject.description}</p>
+                    <div className='project-languages'>
+                      <p><strong>Langages: </strong></p>
+                      {selectedProject.languages.map((language) => (
+                        <p key={language}>{language}</p>
+                      ))}
+                    </div>
+                    <p><strong>Ajouté: </strong>{formatDate(selectedProject.created_at)}</p>
+                    <p><strong>Dernière modification: </strong>{formatDate(selectedProject.updated_at)}</p>
+                  </>
+                ) : (
+                  <p>Sélectionnez un projet pour voir les détails</p>
+                )}
+                </>
+              )}
             </div>
             <div className='project-list'>
-                {search ? (
+              {errorApi ? (
+                <>
+                  {search ? (
                   <>
                     {filteredProjects.length > 0 ? (
                       <>
                         {filteredProjects.map((project) => {
                           return <div key={project.id} className='project' onClick={() => setSelectedProject(project)}>
-                            <img src={`http://51.75.122.193${project.imageUrl}`} alt={project.name} className='project-pic'></img>
+                            <img src={project.imageURL} alt={project.name} className='project-pic'></img>
                             <div className='project-value'>
                                 <p>{project.name}</p>
                                 <div className='project-languages'>
@@ -102,7 +140,7 @@ function Project() {
                   <>
                     {displayProjects ? displayProjects.map((project) => {
                     return <div key={project.id} className='project' onClick={() => setSelectedProject(project)}>
-                        <img src='./Image/shiona.png' alt='Shiona' className='project-pic'></img>
+                        <img src={project.imageURL} alt={project.name} className='project-pic'></img>
                         <div className='project-value'>
                             <p>{project.name}</p>
                             <div className='project-languages'>
@@ -117,6 +155,53 @@ function Project() {
                 )}
                   </>
                 )}
+                </>
+              ) : (
+                <>
+                  {search ? (
+                    <>
+                      {filteredProjects.length > 0 ? (
+                        <>
+                          {filteredProjects.map((project) => {
+                            return <div key={project.id} className='project' onClick={() => setSelectedProject(project)}>
+                              <img src={`http://51.75.122.193${project.imageUrl}`} alt={project.name} className='project-pic'></img>
+                              <div className='project-value'>
+                                  <p>{project.name}</p>
+                                  <div className='project-languages'>
+                                      {project.languages.map((language) => {
+                                          return <p key={language}>{language}</p>
+                                      })}
+                                  </div>
+                              </div>
+                            </div>
+                          })}
+                        </>
+                      ) : (
+                      <>
+                        <p>Aucun projet ne correspond à votre recherche</p>
+                      </>)}
+                    </>
+                  ) : (
+                    <>
+                      {displayProjects ? displayProjects.map((project) => {
+                      return <div key={project.id} className='project' onClick={() => setSelectedProject(project)}>
+                          <img src='./Image/shiona.png' alt='Shiona' className='project-pic'></img>
+                          <div className='project-value'>
+                              <p>{project.name}</p>
+                              <div className='project-languages'>
+                                  {project.languages.map((language) => {
+                                      return <p key={language}>{language}</p>
+                                  })}
+                              </div>
+                          </div>
+                      </div>
+                      }) : (
+                      <p>Pas de projet à montrer</p>
+                  )}
+                    </>
+                  )}
+                </>
+              )}
             </div>
         </div>
     </div>
